eb5194934fd54d2fdc7eac8c0537315056f97ec2
Chun Ki Siu <chunkisiu@Chuns-MacBook-Pro.local>
2024-09-07 10:53:18 +1000

    new template

--- src/auth.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const isAuthenticated = localStorage.getItem("isAuthenticated");
-    if (!isAuthenticated) {
-        window.location.href = "login.html";
-    }
-});
